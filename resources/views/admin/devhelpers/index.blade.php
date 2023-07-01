3a2ab01a41f504b8df98ca5e0d550d45a2a434e2
Massimiliano-Novello <massinov5@gmail.com>
2023-07-01 12:17:27 +0200

    edited route to dashboard

--- resources/views/admin/devhelpers/index.blade.php
+++ resources/views/admin/devhelpers/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-    
+    <h2>ciao</h2>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -62,8 +62,8 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.projects.index' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.projects.index') }}">
-                                    <i class="fa-regular fa-folder-open"></i> Post
+                                    href="{{ route('admin.devhelpers.index') }}">
+                                    <i class="fa-regular fa-folder-open"></i> Programmatori
                                 </a>
                             </li>
                         </ul>
