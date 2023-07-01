87e50b92a1bc6b080d6b56cc0afd49ad82871581
Massimiliano-Novello <massinov5@gmail.com>
2023-07-01 20:16:26 +0200

    add index.blade

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.admin')
-
-@section('content')
-    <h1>HELLO</h1>
-@endsection
--- resources/views/admin/devhelpers/index.blade.php
+++ resources/views/admin/devhelpers/index.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>ciao</h2>
+<div class="container">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Cognome</th>
+            <th scope="col">Email</th>
+            <th scope="col">Github</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Indirizzo</th>
+            <th scope="col">Skills</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($user as $item)
+          <tr>         
+            <td>{{ $item->name }}</td>
+            <td>{{ $item->surname }}</td>
+            <td>{{ $item->email }}</td>
+            <td>{{ $item->github }}</td>
+            <td>{{ $item->phone }}</td>
+            <td>{{ $item->address }}</td>
+            <td>{{ $item->skills }}</td>
+        </tr>
+        @endforeach
+        </tbody>
+      </table>
+</div>
 @endsection
