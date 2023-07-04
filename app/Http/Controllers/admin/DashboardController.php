a654a81ab6beb6e15f9f3f0883d7fb2cb9608e7f
nespolisimmaco <snespoli@yahoo.com>
2023-07-04 16:48:08 +0200

    Try to update user data

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class DashboardController extends Controller
 {
     public function index () {
         $user = Auth::user();
         return view('admin.index', compact('user'));
+    }
+
+
+    public function edit()
+    {
+        return view('profile.complete');
+    }
+
+    
+    public function update(Request $request, User $user)
+    {
+        // $user = Auth::user();
+        // dd($request);
+        // dd($user);
+        $user->update($request->all());
+
+        return Redirect::route('admin.dashboard')->with('status', 'profile-updated');
     }
 }
---
+++ resources/views/profile/complete.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <form method="POST" action="{{ route('admin.dashboard.update') }}" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <h1>Completa profilo</h1>
+
+            {{-- Github --}}
+            <div class="mb-4 row">
+                <label for="github" class="col-md-4 col-form-label text-md-right">{{ __('github') }}</label>
+
+                <div class="col-md-6">
+                    <input id="github" type="text" class="form-control" name="github" value="{{ old('github') }}">
+
+                </div>
+            </div>
+            {{-- End  Github --}}
+
+            {{-- Photo --}}
+            <div class="mb-4 row">
+                <label for="photo" class="col-md-4 col-form-label text-md-right">{{ __('photo') }}</label>
+
+                <div class="col-md-6">
+                    <input id="photo" type="file" class="form-control" name="photo" value="{{ old('photo') }}">
+                </div>
+            </div>
+            {{-- End Photo --}}
+
+            {{-- Phone --}}
+            <div class="mb-4 row">
+                <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('phone') }}</label>
+
+                <div class="col-md-6">
+                    <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+                </div>
+            </div>
+            {{-- End Phone --}}
+
+            {{-- Description --}}
+            <div class="mb-4 row">
+                <label for="desrcription" class="col-md-4 col-form-label text-md-right">{{ __('description') }}</label>
+
+                <div class="col-md-6">
+                    <textarea name="description" id="description"rows="10"></textarea>
+                </div>
+            </div>
+            {{-- End Description --}}
+
+            {{-- Skills --}}
+            <div class="mb-4 row">
+                <label for="skills" class="col-md-4 col-form-label text-md-right">{{ __('skills') }}</label>
+
+                <div class="col-md-6">
+                    <textarea name="skills" id="skills"></textarea>
+                </div>
+            </div>
+            {{-- End Skills --}}
+
+            {{-- Button --}}
+            <div class="mb-4 row mb-0">
+                <div class="text-end mt-2">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Salva') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+@endsection
