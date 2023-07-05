661bc0ac4098a2fe29cc17b006803a58a91cca7f
Massimiliano-Novello <massinov5@gmail.com>
2023-07-05 12:50:44 +0200

    add fixed bug

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,7 +25,6 @@ class ProfileUpdateRequest extends FormRequest
             'description' => ['nullable', 'string'],
             'skills' => ['required', 'string'],
             'email' => ['email:rfc,dns', 'required', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'technologies' => ['required'],
         ];
     }
 
