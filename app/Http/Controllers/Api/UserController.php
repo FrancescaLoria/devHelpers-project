9b68f66824cc03fe566521435e5878436cdf364e
nespolisimmaco <snespoli@yahoo.com>
2023-07-06 22:51:50 +0200

    Fixed many to many relation (for filter)

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,21 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Technology;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index(Request $request){
-        // dd($request->technology_id);
         if ($request->has('technology_id')) {
             $id = $request->technology_id;
-            $developers = User::whereHas(['technologies' => function($query) use($id) {
+            $developers = User::with(['technologies'])->whereHas('technologies', function (Builder $query) use($id) {
                 $query->where('id', $id);
-            }])->get();
-        }else{
-            $developers = 'sei stronzo non ci sono';
+            })->get();
+        } else {
+            $developers = User::all();
         }
+        
         return response()->json([
             'success' => true,
             'results' => $developers
