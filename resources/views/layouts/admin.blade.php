80ee3cb99c3c4860566dd163dd4a14be93c7101a
Paolacocc <coccettipaola@gmail.com>
2023-07-14 17:38:38 +0200

    change logo route

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -29,7 +29,7 @@
         <header class="navbar navbar-dark sticky-top bkg-light
          flex-md-nowrap p-2 shadow">
             <div class="row justify-content-between">
-                <a class="text-dark navbar-brand col-md-3 col-lg-2 me-0 px-3" href="http://localhost:5176/">Devhelpers</a>
+                <a class="text-dark navbar-brand col-md-3 col-lg-2 me-0 px-3" href="http://localhost:5174/">Devhelpers</a>
                 <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
                     data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
                     aria-expanded="false" aria-label="Toggle navigation">
